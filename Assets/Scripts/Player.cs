39482f54f75fbd050ec8ac0449c3b5e58fe83434
VVRauhala <Vili-Veikko.Rauhala@hotmail.com>
2018-04-05 22:15:21 +0300

    Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    private float speed;
+    public float speed;
 
 	// Use this for initialization
 	void Start () {
-        speed = 3;
 	}
 	
 	// Update is called once per frame
