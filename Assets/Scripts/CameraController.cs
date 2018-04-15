f5f5de62df9bbc5b3ef3b4661f94c76511afed51
VVRauhala <Vili-Veikko.Rauhala@hotmail.com>
2018-04-15 11:11:56 +0300

    Better movement. Camera following player. Added map + some monkeys.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject followTarget;
+    private Vector3 targetPosition;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float speed;
+    public float moveSpeed;
+    private Animator anim;
+    private Rigidbody2D prb2d;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float axisX = Input.GetAxis("Horizontal");
-        float axisY = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed);
-	}
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        prb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.05f)
+        {
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            prb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, prb2d.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.05f)
+        {
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            prb2d.velocity = new Vector2(prb2d.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            prb2d.velocity = new Vector2(0f, prb2d.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            prb2d.velocity = new Vector2(prb2d.velocity.x, 0f);
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+    }
 }
