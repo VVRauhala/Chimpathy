7cbc4d793d1cc3d636f144b06a979e858e604c93
VVRauhala <Vili-Veikko.Rauhala@hotmail.com>
2018-05-06 21:34:28 +0300

    Added proper sprite for a player. Made changes on map, added more stuff, added borders. Fixer UI screens and their scalings. Made a first build of the game to main folder.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,22 +7,32 @@ public class Player : MonoBehaviour {
     public float moveSpeed;
     private Animator anim;
     private Rigidbody2D prb2d;
+    private SpriteRenderer spriteRender;
 
 
     private void Start() {
         anim = GetComponent<Animator>();
         prb2d = GetComponent<Rigidbody2D>();
+        spriteRender = GetComponent<SpriteRenderer>();
     }
 
     private void Update() {
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.05f) {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        // Horizontal movement, different for both for flip
+        if (Input.GetAxisRaw("Horizontal") > 0.5f) {
+            spriteRender.flipX = true;
             prb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, prb2d.velocity.y);
         }
+        if (Input.GetAxisRaw("Horizontal") < -0.05f)
+        {
+            spriteRender.flipX = false;
+            prb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, prb2d.velocity.y);
+        }
+        // Vertical movement
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.05f) {
-            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
             prb2d.velocity = new Vector2(prb2d.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
         }
+
+        // Smoothing walking
         if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f) {
             prb2d.velocity = new Vector2(0f, prb2d.velocity.y);
         }
