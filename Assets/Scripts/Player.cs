8b2e3e7c3f29a8600f1c22b0e737a35a174a2f00
VVRauhala <Vili-Veikko.Rauhala@hotmail.com>
2018-05-13 22:28:47 +0300

    Hunger gauge lowers 1 point for every 2 seconds. Old build

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
@@ -8,17 +9,28 @@ public class Player : MonoBehaviour {
     private Animator anim;
     private Rigidbody2D prb2d;
     private SpriteRenderer spriteRender;
+    private float hunger;
+    public Text hungerText;
 
 
     private void Start() {
+        hunger = 100f;
         anim = GetComponent<Animator>();
         prb2d = GetComponent<Rigidbody2D>();
         spriteRender = GetComponent<SpriteRenderer>();
+        StartCoroutine("LowerHunger");
     }
 
-    private void Update() {
+    private void Update()
+    {
+        // lowering hunger level
+        if (hunger > 0)
+        {
+           
+        }
         // Horizontal movement, different for both for flip
-        if (Input.GetAxisRaw("Horizontal") > 0.5f) {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f)
+        {
             spriteRender.flipX = true;
             prb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, prb2d.velocity.y);
         }
@@ -28,19 +40,40 @@ public class Player : MonoBehaviour {
             prb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, prb2d.velocity.y);
         }
         // Vertical movement
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.05f) {
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.05f)
+        {
             prb2d.velocity = new Vector2(prb2d.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
         }
 
         // Smoothing walking
-        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f) {
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
             prb2d.velocity = new Vector2(0f, prb2d.velocity.y);
         }
-        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f) {
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
             prb2d.velocity = new Vector2(prb2d.velocity.x, 0f);
         }
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+    }
+
+    // lowers hunger gauge 1 point for every 2 seconds.
+    IEnumerator LowerHunger()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        hunger = hunger - 1;
+        hungerText.text = hunger.ToString() + " %";
+        if (hunger > 0)
+        {
+            Debug.Log("hunger = " + hunger);
+            StartCoroutine("LowerHunger");
+        }
+        else
+        {
+            Debug.Log("hunger = " + hunger);
+            Debug.Log("Died");
+        }
     }
 }
