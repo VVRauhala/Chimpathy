62d77638ec1996906b693a35a4692d0ab1c8b56a
VVRauhala <Vili-Veikko.Rauhala@hotmail.com>
2018-05-20 18:20:54 +0300

    Simple dialogue

---
+++ Assets/Scripts/DialogHolder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogHolder : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueManager dMan;
+    private bool playerEnter;
+
+	// Use this for initialization
+	void Start () {
+        dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space) && playerEnter)
+        {
+            dMan.ShowBox(dialogue);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            playerEnter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            playerEnter = false;
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dBox;
+    public Text dText;
+    public bool dActive;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dActive && Input.GetKeyDown(KeyCode.K))
+        {
+            dBox.SetActive(false);
+            dActive = false;
+        }
+	}
+
+    public void ShowBox(string dialog)
+    {
+        dActive = true;
+        dBox.SetActive(true);
+        dText.text = dialog;
+    }
+}
